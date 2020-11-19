6f8f043d9e66a21fc9698246a0065933fc7828ad
Farras Hakim <farrashakim@gmail.com>
2020-11-19 21:27:41 +0700

    Remove Debug Logger from ApiClient file

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/farras/pokecarddemo/api/ApiClient.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/farras/pokecarddemo/api/ApiClient.java
@@ -8,16 +8,12 @@ public class ApiClient {
     public static final String BASE_URL = "https://api.pokemontcg.io/v1/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
-        Log.d("ApiClientDebugger", "Awal get client");
         if (retrofit==null) {
-            Log.d("ApiClientDebugger", "if null");
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
-        Log.d("ApiClientDebugger", "akhir");
         return retrofit;
     }
-
 }
