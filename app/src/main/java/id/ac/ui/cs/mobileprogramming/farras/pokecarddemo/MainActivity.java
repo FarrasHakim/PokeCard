61e89d84346061c4044c07365df4306d5e286213
Muhammad Farras hakim <farrashakim@gmail.com>
2020-12-28 17:51:01 +0700

    Implement pint 6

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/farras/pokecarddemo/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/farras/pokecarddemo/MainActivity.java
@@ -1,19 +1,59 @@
 package id.ac.ui.cs.mobileprogramming.farras.pokecarddemo;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    WifiManager mWifiManager;
+    TextView wifiList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (mWifiManager.isWifiEnabled() == false)
+        {
+            // If wifi disabled then enable it
+            Toast.makeText(this, "wifi is disabled..making it enabled",
+                    Toast.LENGTH_LONG).show();
+            mWifiManager.setWifiEnabled(true);
+        }
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -23,6 +63,59 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(mWifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            Log.d("BRReceiver", "Receive Wifi Scan");
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                wifiList = (TextView) findViewById(R.id.article);
+                List<ScanResult> mScanResults = mWifiManager.getScanResults();
+                for (ScanResult result : mScanResults) {
+                    wifiList.setText(result.SSID);
+                }
+            }
+        }
+    };
+
+    public void onClick(View v) {
+        Toast.makeText(this, "Masuk gan", Toast.LENGTH_LONG).show();
+        boolean successScan = mWifiManager.startScan();
+        registerReceiver(mWifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        String textMessage = "Success Scan: " + successScan + "\n"
+                + "Wifi Info: " + mWifiManager.getConnectionInfo() + "\n"
+                + "Wifi State: " + mWifiManager.getWifiState() + "\n"
+                + "Wifi Scan Results: " + mWifiManager.getScanResults().size() + "\n"
+                + "Wifi Connection State: " + mWifiManager.getWifiState();
+
+//        TextView textView = findViewById(R.id.wifiState);
+//        textView.setText(textMessage);
+        Toast.makeText(this, textMessage, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/farras/pokecarddemo/fragment/WifiListFragment.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.mobileprogramming.farras.pokecarddemo.fragment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import id.ac.ui.cs.mobileprogramming.farras.pokecarddemo.R;
+
+import java.util.List;
+
+public class WifiListFragment extends Fragment {
+
+    public WifiListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wifi_list, container, false);
+    }
+
+
+}
